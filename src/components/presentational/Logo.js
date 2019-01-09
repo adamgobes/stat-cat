b19e16d2a0e6af834bbfa133127147e567a4ef50
Adam Gobran <adamgobes@gmail.com>
2019-01-09 15:40:31 -0500

    first commit

---
+++ src/components/presentational/Logo.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../../styles/Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="container logo-container">
+            <div className="head">
+                <div className="stripe one"></div>
+                <div className="stripe two"></div>
+                <div className="stripe three"></div>
+                <div className="stripe four"></div>
+                <div className="stripe five"></div>
+                <div className="stripe six"></div>
+                <div className="stripe seven"></div>
+                <div className="stripe eight"></div>
+                <div className="stripe nine"></div>
+                <div className="ear">
+                    <div className="inner"></div>
+                </div>
+                <div className="ear right">
+                    <div className="inner right"></div>
+                </div>
+                <div className="face">
+
+
+                    <div className="eye">
+                    </div>
+                    <div className="eye right"> </div>
+                    <div className="nose"> </div>
+                    <div className="mouth"> </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;

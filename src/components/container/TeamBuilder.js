06279159af690e4e0fd577368ec6cf4def17d6af
Adam Gobran <adamgobes@gmail.com>
2019-01-10 12:20:02 -0500

    add initial request to fetch all players

--- src/components/container/TeamBuilder.js
+++ src/components/container/TeamBuilder.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Box, Button, TextInput } from 'grommet';
-import styled from 'styled-components';
+import base64 from 'base-64';
+import axios from 'axios';
 
-const AddPlayerInput = styled(TextInput)`
-    width: 600px;
-`;
+class TeamBuilder extends Component {
+    state = {
+        playerInput: '',
+		allPlayers: [],
+		autocomplete: []
+    };
 
-const TeamBuilder = () => (
-    <div>
-        <h1>Let's Build Your Team</h1>
-        <Box align="center">
-            <Box direction="row" width="medium">
-                <TextInput size="large" />
-                <Button label="Add" />
-            </Box>
-        </Box>
-    </div>
-);
+    componentDidMount() {
+        const username = 'adamgobes';
+        const password = 'MYSPORTSFEEDS';
+        const pass = base64.encode(`${username}:${password}`);
+        const config = {
+            headers: { Authorization: `Basic ${pass}` }
+        };
+        axios
+            .get(
+                'https://api.mysportsfeeds.com/v2.0/pull/nba/players.json',
+                config
+            )	
+            .then(res =>
+                this.setState({
+					allPlayers: res.data.players.map(p => p.player),
+					isLoading: false
+                })
+            );
+    }
+
+    handlePlayerInput = e => {
+        this.setState({
+            playerInput: e.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Let's Build Your Team</h1>
+                <Box align="center">
+                    <Box direction="row" width="medium">
+                        <TextInput
+                            size="large"
+                            onChange={this.handlePlayerInput}
+                        />
+                        <Button label="Add" />
+                    </Box>
+                </Box>
+            </div>
+        );
+    }
+}
 
 export default TeamBuilder;

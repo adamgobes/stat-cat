67792d0b5feb8e2cce2e166b3656b27754b7f646
Adam Gobran <adamgobes@gmail.com>
2019-01-11 14:17:56 -0500

    logic to filter players based on text input + add players

--- src/components/container/TeamBuilder.js
+++ src/components/container/TeamBuilder.js
@@ -6,8 +6,9 @@ import axios from 'axios';
 class TeamBuilder extends Component {
     state = {
         playerInput: '',
-		allPlayers: [],
-		autocomplete: []
+        allPlayers: [],
+		autocomplete: [],
+		team: []
     };
 
     componentDidMount() {
@@ -21,20 +22,50 @@ class TeamBuilder extends Component {
             .get(
                 'https://api.mysportsfeeds.com/v2.0/pull/nba/players.json',
                 config
-            )	
+            )
             .then(res =>
                 this.setState({
-					allPlayers: res.data.players.map(p => p.player),
-					isLoading: false
+                    allPlayers: res.data.players.map(p => p.player),
+                    isLoading: false
                 })
             );
     }
 
-    handlePlayerInput = e => {
+    handlePlayerInputChange = e => {
+        const val = e.target.value;
         this.setState({
-            playerInput: e.target.value
+            playerInput: val,
+            autocomplete: this.state.allPlayers
+                .filter(
+                    player =>
+                        player.firstName
+                            .toLowerCase()
+                            .startsWith(val.toLowerCase()) ||
+                        player.lastName
+                            .toLowerCase()
+                            .startsWith(val.toLowerCase())
+                )
+                .map(player => `${player.firstName} ${player.lastName}`)
         });
-    };
+	};
+
+	handleAddPlayer = playerName => {
+		const firstName = playerName.substring(0, playerName.indexOf(" "))
+		const lastName = playerName.substring(playerName.indexOf(" ") + 1)
+
+		const playerObject = this.state.allPlayers.filter(p => p.firstName === firstName && p.lastName === lastName)[0]
+
+		this.setState(prevState => ({
+			team: [...prevState.team, playerObject],
+			playerInput: ''
+		}))
+	}
+	
+	handlePlayerSelect = e => {
+		this.setState({
+			playerInput: e.suggestion
+		})
+	}
 
     render() {
         return (
@@ -43,12 +74,22 @@ class TeamBuilder extends Component {
                 <Box align="center">
                     <Box direction="row" width="medium">
                         <TextInput
-                            size="large"
-                            onChange={this.handlePlayerInput}
+							size="large"
+							value={this.state.playerInput}
+                            onChange={this.handlePlayerInputChange}
+							suggestions={this.state.autocomplete}
+							onSelect={this.handlePlayerSelect}
                         />
-                        <Button label="Add" />
+                        <Button label="Add" onClick={() => this.handleAddPlayer(this.state.playerInput)}/>
                     </Box>
                 </Box>
+				<Box>
+					<ul>
+						{this.state.team.map(player => (
+							<li key={player.id}>{`${player.firstName} ${player.lastName}`}</li>
+						))}
+					</ul>
+				</Box>
             </div>
         );
     }

b1d5f1140acf58d5027904229520ff19c0dc5ff1
Adam Gobran <adamgobes@gmail.com>
2019-01-13 20:57:44 -0500

    add new split screen design

--- src/components/container/TeamBuilder.js
+++ src/components/container/TeamBuilder.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Box } from 'grommet'
+import { Box, Grid } from 'grommet'
 import styled from 'styled-components'
 
 import allPlayersRequest from '../../utils/sportsFeedAPI'
 import AddPlayerInput from '../presentational/TeamBuilder/AddPlayerInput'
 import TeamTable from '../presentational/TeamBuilder/TeamTable'
+import PlayersGrid from '../presentational/TeamBuilder/PlayersGrid'
 import Logo from '../presentational/Logo'
 
 const Header = styled.h1`
     text-align: center;
     margin: 60px 0;
+`
+
+const PlayersGridWrapper = styled.div`
+    overflow: scroll;
 `
 
 const TeamBuilder = () => {
@@ -19,60 +24,72 @@ const TeamBuilder = () => {
     const [team, setTeam] = useState([])
 
     useEffect(() => {
-        allPlayersRequest().then(res => setAllPlayers(res.data.players.map(p => p.player)))
+        allPlayersRequest().then(res => setAllPlayers(
+                res.data.players.map(p => ({
+                    firstName: p.player.firstName,
+                    lastName: p.player.lastName,
+                    fullName: `${p.player.firstName} ${p.player.lastName}`,
+                    currentTeam: p.player.currentTeam ? p.player.currentTeam.abbreviation : 'None',
+                    imageSrc: p.player.officialImageSrc,
+                    id: p.player.id,
+                })),
+            ))
     })
-
     const onPlayerInputChange = (e) => {
         const val = e.target.value
 
         setPlayerInput(val)
-        setAutocomplete(
-            allPlayers
-                .filter(
-                    player => player.firstName.toLowerCase().startsWith(val.toLowerCase())
+
+        if (val === '') setAutocomplete([])
+        else {
+            setAutocomplete(
+                allPlayers.filter(
+                    player => player.fullName.toLowerCase().startsWith(val.toLowerCase())
+                        || player.firstName.toLowerCase().startsWith(val.toLowerCase())
                         || player.lastName.toLowerCase().startsWith(val.toLowerCase()),
-                )
-                .map(player => `${player.firstName} ${player.lastName}`),
-        )
+                ),
+            )
+        }
     }
 
-    const onAddPlayer = (playerName) => {
-        const firstName = playerName.substring(0, playerName.indexOf(' '))
-        const lastName = playerName.substring(playerName.indexOf(' ') + 1)
-
-        const playerObject = allPlayers
-            .filter(p => p.firstName === firstName && p.lastName === lastName)
-            .map(p => ({
-                fullName: `${p.firstName} ${p.lastName}`,
-                currentTeam: p.currentTeam.abbreviation,
-                id: p.id,
-            }))[0]
-
-        setTeam([...team, playerObject])
-        setPlayerInput('')
-        setAutocomplete([])
+    const onAddPlayer = (player) => {
+        setTeam([...team, player])
     }
-
-    const onPlayerSelect = e => setPlayerInput(e.suggestion)
 
     return (
-        <div>
-            <Header>Team Builder</Header>
-            <AddPlayerInput
-                autocomplete={autocomplete}
-                playerInput={playerInput}
-                handleAddPlayer={onAddPlayer}
-                handlePlayerInputChange={onPlayerInputChange}
-                handlePlayerSelect={onPlayerSelect}
-            />
-            {team.length !== 0 && <TeamTable team={team} />}
-            {team.length === 0 && (
-                <Box align="center" pad="large">
-                    <Logo />
-					<h2>Add players using the form above!</h2>
-                </Box>
-            )}
-        </div>
+        <Grid
+            fill
+            areas={[
+                { name: 'search', start: [0, 0], end: [0, 0] },
+                { name: 'team', start: [1, 0], end: [1, 0] },
+            ]}
+            columns={['1/3', 'flex']}
+            rows={['flex']}
+            gap="small"
+        >
+            <Box gridArea="search">
+                <Header>Team Builder</Header>
+                <div>
+                    <AddPlayerInput
+                        playerInput={playerInput}
+                        handleAddPlayer={onAddPlayer}
+                        handlePlayerInputChange={onPlayerInputChange}
+                    />
+                </div>
+                <PlayersGridWrapper>
+                    <PlayersGrid autocomplete={autocomplete} onAddPlayer={onAddPlayer} />
+                </PlayersGridWrapper>
+            </Box>
+            <Box gridArea="team">
+                {team.length !== 0 && <TeamTable team={team} />}
+                {team.length === 0 && (
+                    <Box align="center" pad="large" justify="center" className="container">
+                        <Logo />
+                        <h2>Add players using the form to the left!</h2>
+                    </Box>
+                )}
+            </Box>
+        </Grid>
     )
 }
 

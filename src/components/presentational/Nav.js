59def607461c1081f48bd4a532c3167e0206c6de
Adam Gobran <adamgobes@gmail.com>
2019-01-14 21:09:15 -0500

    modify navbar

--- src/components/presentational/Nav.js
+++ src/components/presentational/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, Menu } from 'grommet'
+import { Box, Button, Menu } from 'grommet'
 
 import Logo from './Logo'
 import StatLogo from '../../assets/images/stat-logo.png'
@@ -8,20 +8,20 @@ import StatLogo from '../../assets/images/stat-logo.png'
 const LogoContainer = styled(Box)`
     width: 60px;
     height: 60px;
-    margin: 0 20px;
+    margin: 0 20px 0 0;
 `
 
 const Nav = () => (
-    <Box direction="row" justify="center" style={{ marginBottom: '40px' }}>
+    <Box direction="row" justify="center" style={{ margin: '36px 0 40px 0' }}>
         <Box direction="row" justify="between" width="xlarge">
-            <Box direction="row">
+            <Box>
+                <Button>Sign Up</Button>
+            </Box>
+			<Box direction="row">
                 <LogoContainer direction="row">
                     <img src={StatLogo} alt="Stat Logo" height="100%" width="100%" />
                 </LogoContainer>
 				<h2 style={{ color: '#e17e62' }}>StatCat</h2>
-            </Box>
-            <Box>
-                <h1>Team Builder</h1>
             </Box>
             <Box>
                 <Menu

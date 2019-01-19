9e4529fc489d54cf1bcc0534df944b8be2d4cb5a
Adam Gobran <adamgobes@gmail.com>
2019-01-19 17:56:37 -0500

    merge server directory into repo

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
+
+const typeDefs = './src/schema.graphql';
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+
+const resolvers = {
+    Query,
+	Mutation,
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: request => ({ ...request, prisma })
+});
+server.start(() => console.log('Server is running on http://localhost:4000'));
---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,11 @@
+const { getUserId } = require('../utils');
+
+function allUsers(parent, args, context) {
+	const id = getUserId(context)
+
+	return context.prisma.users();
+}
+
+module.exports = {
+	allUsers
+}

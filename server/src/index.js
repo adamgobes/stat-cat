5d28f47c71594fa6390a49c74b98991be8e088b6
Adam Gobran <adamgobes@gmail.com>
2019-01-20 20:36:47 -0500

    indent with tabs instead of spaces

--- server/src/index.js
+++ server/src/index.js
@@ -6,13 +6,13 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 
 const resolvers = {
-    Query,
-    Mutation,
+	Query,
+	Mutation,
 }
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
-    context: request => ({ ...request, prisma }),
+	typeDefs,
+	resolvers,
+	context: request => ({ ...request, prisma }),
 })
 server.start(() => console.log('Server is running on http://localhost:4000'))
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -3,38 +3,38 @@ const jwt = require('jsonwebtoken')
 const { APP_SECRET } = require('../utils')
 
 async function register(parents, args, context, info) {
-    const password = await bcrypt.hash(args.password, 10)
+	const password = await bcrypt.hash(args.password, 10)
 
-    const user = await context.prisma.createUser({ ...args, password })
+	const user = await context.prisma.createUser({ ...args, password })
 
-    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+	const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
-    return {
-        token,
-        user,
-    }
+	return {
+		token,
+		user,
+	}
 }
 
 async function login(parent, args, context, info) {
-    const user = await context.prisma.user({ email: args.email })
-    if (!user) {
-        throw new Error('No such user found')
-    }
+	const user = await context.prisma.user({ email: args.email })
+	if (!user) {
+		throw new Error('No such user found')
+	}
 
-    const valid = await bcrypt.compare(args.password, user.password)
-    if (!valid) {
-        throw new Error('Invalid password')
-    }
+	const valid = await bcrypt.compare(args.password, user.password)
+	if (!valid) {
+		throw new Error('Invalid password')
+	}
 
-    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+	const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
-    return {
-        token,
-        user,
-    }
+	return {
+		token,
+		user,
+	}
 }
 
 module.exports = {
-    register,
-    login,
+	register,
+	login,
 }

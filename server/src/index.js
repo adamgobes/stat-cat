56d468e32c83d6e3027b843f95334683c4ba36b2
Adam Gobran <adamgobes@gmail.com>
2019-01-26 12:06:44 -0500

    comment code!!

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,6 @@ const resolvers = {
 const server = new GraphQLServer({
 	typeDefs,
 	resolvers,
-	context: request => ({ ...request, prisma }),
+	context: request => ({ ...request, prisma }), // attach request object (for Authorization) and prisma client to context
 })
 server.start(() => console.log('Server is running on http://localhost:4000'))
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -9,6 +9,7 @@ async function register(parents, args, context, info) {
 
 	const user = await context.prisma.createUser({ ...args, password })
 
+	// initialize user's team when they create an account
 	const initialTeam = {
 		name: `${user.name}'s Team`,
 		owner: { connect: { id: user.id } },
@@ -45,11 +46,14 @@ async function login(parent, args, context, info) {
 
 async function saveTeam(parent, args, context) {
 	const userId = getUserId(context)
-	const teamId = await context.prisma
+	const teamId = await context.prisma // get user's team based on their id
 		.user({ id: userId })
 		.team()
 		.id()
 
+	/* In DB, Team.players is simply array of sportsFeed IDs
+	 * In resolvers/Team.js this array gets mapped to array of actual player objects (see schema.graphql for what that looks like)
+	 */
 	return context.prisma.updateTeam({
 		where: { id: teamId },
 		data: {

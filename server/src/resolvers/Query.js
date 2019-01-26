e803293e2c070bdff85edf0511b4ca579446954b
Adam Gobran <adamgobes@gmail.com>
2019-01-26 12:07:30 -0500

    update player resolver to fetch data from sportsFeed for each id

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,7 +1,4 @@
-const base64 = require('base-64')
-
 const { getUserId } = require('../utils')
-const { sportsFeedUsername, sportsFeedPassword, sportsFeedUrl } = require('../config')
 
 function allUsers(parent, args, context) {
 	const id = getUserId(context)
@@ -15,26 +12,7 @@ function me(parent, args, context) {
 	return context.prisma.user({ id })
 }
 
-function player(parent, args, context) {
-	const pass = base64.encode(`${sportsFeedUsername}:${sportsFeedPassword}`)
-	const config = {
-		headers: { Authorization: `Basic ${pass}` },
-	}
-	return fetch(`${sportsFeedUrl}/player_stats_totals.json?player=${args.id}`, config)
-		.then(res => res.json())
-		.then(json => ({
-			id: json.playerStatsTotals[0].player.id,
-			firstName: json.playerStatsTotals[0].player.firstName,
-			lastName: json.playerStatsTotals[0].player.lastName,
-			fullName: `${json.playerStatsTotals[0].player.firstName} ${
-				json.playerStatsTotals[0].player.lastName
-			}`,
-			position: json.playerStatsTotals[0].player.primaryPosition,
-		}))
-}
-
 module.exports = {
 	allUsers,
 	me,
-	player,
 }

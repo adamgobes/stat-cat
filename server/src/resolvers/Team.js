94ed8748c89ef4e4c1b36829d4c2e2a2dfcdf5eb
Adam Gobran <adamgobes@gmail.com>
2019-01-26 12:07:53 -0500

    comment code + add players resolver

--- server/src/resolvers/Team.js
+++ server/src/resolvers/Team.js
@@ -1,14 +1,35 @@
+const base64 = require('base-64')
+
 const { getUserId } = require('../utils')
 
+const { sportsFeedUsername, sportsFeedPassword, sportsFeedUrl } = require('../config')
+
 function owner(parent, args, context) {
-	console.log('this ran')
 	const id = getUserId(context)
 
 	return context.prisma.team({ id: parent.id }).owner()
 }
 
-function players(parent, args, context) {
-	return parent.players
+// in DB players are stored as IDs, this resolver turns those IDs into actual player objects
+// resolves User.team.players
+function players(parent) {
+	const pass = base64.encode(`${sportsFeedUsername}:${sportsFeedPassword}`)
+	const config = {
+		headers: { Authorization: `Basic ${pass}` },
+	}
+	return parent.players.map(playerId =>
+		fetch(`${sportsFeedUrl}/player_stats_totals.json?player=${playerId}`, config)
+			.then(res => res.json())
+			.then(json => ({
+				id: json.playerStatsTotals[0].player.id,
+				firstName: json.playerStatsTotals[0].player.firstName,
+				lastName: json.playerStatsTotals[0].player.lastName,
+				fullName: `${json.playerStatsTotals[0].player.firstName} ${
+					json.playerStatsTotals[0].player.lastName
+				}`,
+				position: json.playerStatsTotals[0].player.primaryPosition,
+			}))
+	)
 }
 
 module.exports = {

d19bab8be0eb521a58b2e6ee65a319930741f6d0
Adam Gobran <adamgobes@gmail.com>
2019-01-27 16:21:06 -0500

    fix .filter bug

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -21,15 +21,15 @@ function allPlayers(parent, args) {
 	return sportsFeedRequest('player_stats_totals.json')
 		.then(res => res.json())
 		.then(json =>
-			json.playerStatsTotals.map(p =>
-				({
+			json.playerStatsTotals
+				.map(p => ({
 					id: p.player.id,
 					firstName: p.player.firstName,
 					lastName: p.player.lastName,
 					fullName: `${p.player.firstName} ${p.player.lastName}`,
 					position: p.player.primaryPosition,
-				}.filter(pObj => (args.filter ? containsFilter(pObj, args.filter) : true)))
-			)
+				}))
+				.filter(pObj => (args.filter ? containsFilter(pObj, args.filter) : true))
 		)
 }
 

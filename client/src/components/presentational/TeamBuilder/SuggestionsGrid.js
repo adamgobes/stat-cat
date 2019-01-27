e74e716093ab1edebb97682788a0c5d6db673d67
Adam Gobran <adamgobes@gmail.com>
2019-01-27 17:13:42 -0500

    refactor SuggestionGrid to use react-apollo Query component

--- client/src/components/presentational/TeamBuilder/SuggestionsGrid.js
+++ client/src/components/presentational/TeamBuilder/SuggestionsGrid.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Box, Grid } from 'grommet'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
 import PlayerSelectable from './PlayerSelectable'
 
@@ -8,20 +10,43 @@ const StyledBox = styled(Box)`
 	margin-top: 40px;
 	overflow: scroll;
 `
+const ALL_PLAYERS_QUERY = gql`
+	query allPlayersQuery($filter: String) {
+		allPlayers(filter: $filter) {
+			id
+			fullName
+		}
+	}
+`
 
-const SuggestionsGrid = ({ suggestions, onAddPlayer }) => (
+const SuggestionsGrid = ({ filter, onAddPlayer }) => (
 	<StyledBox width="large">
-		<Grid
-			columns={{
-				count: 3,
-				size: 'auto',
-			}}
-			gap={{ row: 'medium', column: 'none' }}
-		>
-			{suggestions.slice(0, 6).map(p => (
-				<PlayerSelectable player={p} handleAddPlayer={onAddPlayer} key={p.id} />
-			))}
-		</Grid>
+		{!filter && <div>Type some shit</div>}
+		{filter && (
+			<Query query={ALL_PLAYERS_QUERY} variables={{ filter }}>
+				{({ loading, error, data }) => {
+					if (loading) return <div>loading</div>
+
+					return (
+						<Grid
+							columns={{
+								count: 3,
+								size: 'auto',
+							}}
+							gap={{ row: 'medium', column: 'none' }}
+						>
+							{data.allPlayers.slice(0, 6).map(p => (
+								<PlayerSelectable
+									key={p.id}
+									player={p}
+									handleAddPlayer={onAddPlayer}
+								/>
+							))}
+						</Grid>
+					)
+				}}
+			</Query>
+		)}
 	</StyledBox>
 )
 

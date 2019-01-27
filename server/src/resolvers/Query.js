1921e7a3a110b4afe82266b16e3402c9b16e32d6
Adam Gobran <adamgobes@gmail.com>
2019-01-27 16:18:17 -0500

    add support for filter functionality for allPlayers query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,6 +1,14 @@
 const { getUserId } = require('../utils')
 const { sportsFeedRequest } = require('../sportsFeed')
 
+function containsFilter(playerObj, filter) {
+	return (
+		playerObj.fullName.toLowerCase().startsWith(filter.toLowerCase()) ||
+		playerObj.firstName.toLowerCase().startsWith(filter.toLowerCase()) ||
+		playerObj.lastName.toLowerCase().startsWith(filter.toLowerCase())
+	)
+}
+
 function me(parent, args, context) {
 	const id = getUserId(context)
 
@@ -8,17 +16,20 @@ function me(parent, args, context) {
 }
 
 // resolver to retrieve all players from third-party SportsFeed datasource
-function allPlayers() {
+// map through response from API, transform into object that matches schema and finally filter if there was one passed (args.filter)
+function allPlayers(parent, args) {
 	return sportsFeedRequest('player_stats_totals.json')
 		.then(res => res.json())
 		.then(json =>
-			json.playerStatsTotals.map(p => ({
-				id: p.player.id,
-				firstName: p.player.firstName,
-				lastName: p.player.lastName,
-				fullName: `${p.player.firstName} ${p.player.lastName}`,
-				position: p.player.primaryPosition,
-			}))
+			json.playerStatsTotals.map(p =>
+				({
+					id: p.player.id,
+					firstName: p.player.firstName,
+					lastName: p.player.lastName,
+					fullName: `${p.player.firstName} ${p.player.lastName}`,
+					position: p.player.primaryPosition,
+				}.filter(pObj => (args.filter ? containsFilter(pObj, args.filter) : true)))
+			)
 		)
 }
 

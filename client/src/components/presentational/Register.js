a3e41b7c2f4211ae72e3861ba201e47884144acc
Adam Gobran <adamgobes@gmail.com>
2019-01-30 19:50:20 -0500

    Register functionality + hook up component to registerMutation

--- client/src/components/presentational/Register.js
+++ client/src/components/presentational/Register.js
@@ -1,21 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
 import { Box, TextInput, Button } from 'grommet'
 import styled, { withTheme } from 'styled-components'
+import gql from 'graphql-tag'
+import cookie from 'react-cookies'
+
+const REGISTER_MUTATION = gql`
+	mutation registerMutation($name: String!, $email: String!, $password: String!) {
+		register(name: $name, email: $email, password: $password) {
+			token
+		}
+	}
+`
 
 const FormInput = styled(TextInput)`
 	width: 300px;
 	margin: 10px 0;
-	color: ${props => props.theme.global.colors.brand} !important;
+	color: ${props => props.theme.global.colors.brand};
 `
 
-function Register() {
+const RegisterButton = styled(Button)`
+	background: ${props => props.theme.global.colors.brand};
+	color: white;
+`
+
+function Register({ history }) {
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [secondPassword, setSecondPassword] = useState('')
+
+	function handleInputChange(setFunction, value) {
+		setFunction(value)
+	}
+
+	function formValid() {
+		return (
+			email.length !== 0 &&
+			name.length !== 0 &&
+			password.length !== 0 &&
+			secondPassword.length !== 0 &&
+			password === secondPassword
+		)
+	}
+
+	function onRegisterComplete({ register }) {
+		const { token } = register
+		cookie.save('authToken', token, { path: '/' })
+		history.push('/teambuilder')
+	}
+
 	return (
 		<Box pad="large" justify="center" align="center" className="container">
 			<Box pad="small">
-				<FormInput size="medium" placeholder="Username" />
-				<FormInput size="medium" placeholder="password" type="password" />
-				<FormInput size="medium" placeholder="Re-enter password" type="password" />
-				<Button label="Register" />
+				<FormInput
+					size="medium"
+					placeholder="Full Name"
+					value={name}
+					onChange={e => handleInputChange(setName, e.target.value)}
+				/>
+				<FormInput
+					size="medium"
+					placeholder="Email"
+					value={email}
+					onChange={e => handleInputChange(setEmail, e.target.value)}
+				/>
+				<FormInput
+					size="medium"
+					placeholder="password"
+					type="password"
+					value={password}
+					onChange={e => handleInputChange(setPassword, e.target.value)}
+				/>
+				<FormInput
+					size="medium"
+					placeholder="Re-enter password"
+					type="password"
+					value={secondPassword}
+					onChange={e => handleInputChange(setSecondPassword, e.target.value)}
+				/>
+				<Mutation
+					mutation={REGISTER_MUTATION}
+					variables={{ name, email, password }}
+					onCompleted={data => onRegisterComplete(data)}
+				>
+					{mutation => (
+						<RegisterButton
+							label="Register"
+							onClick={mutation}
+							style={{ opacity: formValid() ? 1 : 0.5 }}
+							disabled={!formValid()}
+						/>
+					)}
+				</Mutation>
 			</Box>
 		</Box>
 	)

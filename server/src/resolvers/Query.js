34d6d098da22936c16255f6aa2d023f4daf154b2
Adam Gobran <adamgobes@gmail.com>
2019-02-15 12:01:20 -0500

    finish writing behemoth projections query (needs refactoring)

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,7 @@
 const { getUserId } = require('../utils')
 const { sportsFeedRequest, extractBasicInfo } = require('../sportsFeed')
+const { players: playersResolver } = require('./Team')
+const { stats: statsResolver } = require('./Player')
 
 function containsFilter(playerObj, filter) {
 	return (
@@ -27,7 +29,97 @@ function allPlayers(parent, args) {
 		)
 }
 
+function myTeam(parent, args, context) {
+	const id = getUserId(context)
+
+	return context.prisma
+		.user({ id })
+		.team()
+		.players()
+}
+
+function calculateGameCount(teamId, startDate, endDate) {
+	return sportsFeedRequest(
+		`2018-2019-regular/team_gamelogs.json?team=${teamId}&date=from-${startDate}-to-${endDate}`
+	)
+		.then(res => res.json())
+		.then(json => json.gamelogs.length)
+}
+
+function roundAndParse(float) {
+	return parseFloat(float.toFixed(1))
+}
+
+async function projections(parent, args, context) {
+	const { userId, startDate, endDate } = args
+
+	const userPlayers = await context.prisma
+		.user({ id: userId })
+		.team()
+		.players()
+
+	let resolvedPlayers = playersResolver({
+		players: userPlayers,
+	})
+
+	resolvedPlayers = await Promise.all(resolvedPlayers)
+
+	const projectedStats = {}
+
+	const teamToGameCount = {}
+
+	const teamIds = []
+
+	const teamCountPromises = resolvedPlayers.map(player => {
+		const playerTeam = player.currentTeam.id
+		if (!teamIds.includes(playerTeam)) {
+			teamIds.push(playerTeam)
+			return calculateGameCount(playerTeam, startDate, endDate)
+		}
+		return null
+	})
+
+	const teamCounts = await Promise.all(teamCountPromises)
+
+	teamCounts.forEach((count, i) => {
+		teamToGameCount[teamIds[i]] = count
+	})
+
+	const playerStatPromises = resolvedPlayers.map(player => statsResolver({ id: player.id }))
+
+	const allPlayerStats = await Promise.all(playerStatPromises)
+
+	resolvedPlayers.forEach((player, i) => {
+		const playerStats = allPlayerStats[i]
+		playerStats.forEach(stat => {
+			if (
+				stat.category !== 'FGA' &&
+				stat.category !== 'FGM' &&
+				stat.category !== 'FTA' &&
+				stat.category !== 'FTM'
+			) {
+				if (stat.category in projectedStats) {
+					projectedStats[stat.category] = roundAndParse(
+						projectedStats[stat.category] +
+							roundAndParse(stat.value * teamToGameCount[player.currentTeam.id])
+					)
+				} else {
+					projectedStats[stat.category] = roundAndParse(
+						stat.value * teamToGameCount[player.currentTeam.id]
+					)
+				}
+			}
+		})
+	})
+
+	return Object.keys(projectedStats).map(stat => ({
+		category: stat,
+		value: projectedStats[stat],
+	}))
+}
+
 module.exports = {
 	me,
 	allPlayers,
+	projections,
 }

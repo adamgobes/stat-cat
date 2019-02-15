f8dd79559b7f60d118f02d9669bfcdf91ca05c72
Adam Gobran <adamgobes@gmail.com>
2019-02-15 13:49:32 -0500

    render ProjectionsTable in Dashboard component

--- client/src/components/container/Dashboard.js
+++ client/src/components/container/Dashboard.js
@@ -5,6 +5,7 @@ import { compose } from 'recompose'
 
 import Nav from '../presentational/Nav'
 import TopPerformers from '../presentational/Dashboard/TopPerformers'
+import ProjectionTable from '../presentational/Dashboard/ProjectionTable'
 
 const Dashboard = () => {
 	return (
@@ -12,6 +13,7 @@ const Dashboard = () => {
 			<Nav showMenu showSignUp={false} />
 			<h1 style={{ textAlign: 'center' }}>Your Dashboard</h1>
 			<TopPerformers />
+			<ProjectionTable />
 		</Box>
 	)
 }

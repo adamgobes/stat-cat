1089e430a38cc2b2771a5c3fb7d08405f5911665
Adam Gobran <adamgobes@gmail.com>
2019-02-16 17:42:09 -0500

    initialize injury report component

--- client/src/components/container/Dashboard.js
+++ client/src/components/container/Dashboard.js
@@ -6,6 +6,7 @@ import { compose } from 'recompose'
 import Nav from '../presentational/Nav'
 import TopPerformers from '../presentational/Dashboard/TopPerformers'
 import ProjectionTable from '../presentational/Dashboard/ProjectionTable'
+import InjuryReport from '../presentational/Dashboard/InjuryReport'
 
 const Dashboard = () => {
 	return (
@@ -14,6 +15,7 @@ const Dashboard = () => {
 			<h1 style={{ textAlign: 'center' }}>Your Dashboard</h1>
 			<TopPerformers />
 			<ProjectionTable />
+			<InjuryReport />
 		</Box>
 	)
 }
---
+++ client/src/components/presentational/InjuryReport.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from 'grommet'
+import { graphql } from 'react-apollo'
+import { compose } from 'recompose'
+
+const InjuryReport = () => {
+
+	return <div>Injuries</div>
+
+}
+
+export default InjuryReport

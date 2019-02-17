18d7fe590cebc3c10027ea36e10dea3a7d28d1b0
Adam Gobran <adamgobes@gmail.com>
2019-02-17 18:06:59 -0500

    remove client specific queries

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -2,20 +2,12 @@ import gql from 'graphql-tag'
 
 import BasicPlayerInfoFragment from './fragments'
 
-// query to fetch user's info to initialize apollo cache
-export const ME_QUERY = gql`
+export const MY_TEAM_QUERY = gql`
 	${BasicPlayerInfoFragment}
 	query {
-		me {
-			id
-			team {
-				players {
-					...BasicPlayerInfo
-					stats {
-						category
-						value
-					}
-				}
+		myTeam {
+			players {
+				...BasicPlayerInfo
 			}
 		}
 	}
@@ -47,25 +39,5 @@ export const ALL_PLAYERS_QUERY = gql`
 		allPlayers(filter: $filter) {
 			...BasicPlayerInfo
 		}
-	}
-`
-
-export const USER_TEAM_QUERY = gql`
-	${BasicPlayerInfoFragment}
-	query {
-		userTeam @client {
-			...BasicPlayerInfo
-			stats {
-				category
-				value
-			}
-		}
-	}
-`
-
-export const LOGGED_IN_QUERY = gql`
-	${BasicPlayerInfoFragment}
-	query {
-		loggedIn @client
 	}
 `

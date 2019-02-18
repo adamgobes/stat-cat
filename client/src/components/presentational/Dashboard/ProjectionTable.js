72920edf71ce71051c1acfab05733bd402499299
Adam Gobran <adamgobes@gmail.com>
2019-02-18 13:13:03 -0500

    use DataTable for projections

--- client/src/components/presentational/Dashboard/ProjectionTable.js
+++ client/src/components/presentational/Dashboard/ProjectionTable.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { Box, DataTable, Text } from 'grommet'
 
 import { computeProjections } from '../../../utils/computeHelpers'
 
 const ProjectionTable = ({ projectionsData }) => {
 	const { players } = projectionsData
 	const projections = computeProjections(players)
-	return projections.map(p => <div key={p.category}>{`${p.category}:${p.value}`}</div>)
+	return (
+		<Box>
+			<DataTable
+				columns={[
+					{
+						property: 'stat',
+						header: <Text>Category</Text>,
+						primary: true,
+					},
+					{
+						property: 'value',
+						header: <Text>Projection</Text>,
+						render: data => <Text>{data.value}</Text>,
+					},
+				]}
+				data={projections.map(p => ({
+					stat: p.category,
+					value: p.value,
+				}))}
+			/>
+		</Box>
+	)
 }
 
 export default ProjectionTable

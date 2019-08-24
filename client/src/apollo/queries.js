87996cab268f6706ef64dd36ff7ce3d7ecacbc68
Adam Gobran <adamgobes@gmail.com>
2019-08-24 15:57:31 +0100

    refactor

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -33,7 +33,7 @@ export const DASHBOARD_QUERY = gql`
     }
 `
 
-export const PLAYERS_SEARCH_QUERY = gql`
+export const SEARCH_PLAYERS_QUERY = gql`
     ${BasicPlayerInfoFragment}
     query allPlayersQuery($filter: String) {
         allPlayers(filter: $filter) {

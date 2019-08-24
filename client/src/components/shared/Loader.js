a6f1cbd7b0046606f2ac65691fde0a2ed71e49d8
Adam Gobran <adamgobes@gmail.com>
2019-08-24 15:15:18 +0100

    refactor

--- client/src/components/shared/Loader.js
+++ client/src/components/shared/Loader.js
@@ -1,9 +1,8 @@
-import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
 const rotate = keyframes`
-	0% { transform: rotate(0deg); }
-	100% { transform: rotate(360deg); }
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
 `
 
 const Loader = styled.div`

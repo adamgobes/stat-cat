f0ab3a59aebfe4f146e6a0829aea314ab55de8b2
Adam Gobran <adamgobes@gmail.com>
2019-08-28 11:29:40 +0100

    fiddle around with dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import { Box, Grid } from 'grommet'
-import { compose } from 'recompose'
 import styled from 'styled-components'
 
 import Nav from '../general/Nav'
-import TopPerformers from './TopPerformers'
-import ProjectionTable from './ProjectionTable'
-import InjuryReport from './InjuryReport'
-import { DASHBOARD_QUERY } from '../../apollo/queries'
 
 const WeeklyReportWrapper = styled(Box)`
     box-shadow: rgba(0, 0, 0, 0.3) 0 1px 6px;
@@ -22,30 +17,35 @@ const TopPerformersWrapper = styled(Box)`
     box-shadow: rgba(0, 0, 0, 0.3) 0 1px 6px;
 `
 
+const SomeBox = styled(Box)`
+    height: 200px;
+    width: 200px;
+`
+
 const Dashboard = () => (
     <Box>
         <Nav />
-        {/* <Nav showMenu showSignUp={false} />
-            <Grid
-                rows={['1/2', '1/2']}
-                columns={['2/5', 'flex']}
-                gap="small"
-                areas={[
-                    { name: 'weeklyReport', start: [0, 0], end: [0, 1] },
-                    { name: 'projections', start: [1, 0], end: [1, 0] },
-                    { name: 'topPerformers', start: [1, 1], end: [1, 1] },
-                ]}
-            >
-                <WeeklyReportWrapper gridArea="weeklyReport">
-                    <InjuryReport injuriesData={myTeam} />
-                </WeeklyReportWrapper>
-                <ProjectionTableWrapper gridArea="projections">
-                    <ProjectionTable projectionsData={myTeam} />
-                </ProjectionTableWrapper>
-                <TopPerformersWrapper gridArea="topPerformers">
-                    <TopPerformers topPerformersData={myTeam} />
-                </TopPerformersWrapper>
-            </Grid> */}
+        <Grid
+            style={{ margin: '0 40px' }}
+            rows={['1/2', '1/2']}
+            columns={['2/5', 'flex']}
+            gap="small"
+            areas={[
+                { name: 'weeklyReport', start: [0, 0], end: [0, 1] },
+                { name: 'projections', start: [1, 0], end: [1, 0] },
+                { name: 'topPerformers', start: [1, 1], end: [1, 1] },
+            ]}
+        >
+            <WeeklyReportWrapper gridArea="weeklyReport">
+                <SomeBox />
+            </WeeklyReportWrapper>
+            <ProjectionTableWrapper gridArea="projections">
+                <SomeBox />
+            </ProjectionTableWrapper>
+            <TopPerformersWrapper gridArea="topPerformers">
+                <SomeBox />
+            </TopPerformersWrapper>
+        </Grid>
     </Box>
 )
 

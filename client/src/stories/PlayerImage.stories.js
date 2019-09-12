d753c26e4dc1e883209c843010459d30f5dc2b3b
Adam Gobran <adamgobes@gmail.com>
2019-09-12 18:18:54 -0400

    PlayerImage story

---
+++ client/src/stories/PlayerImage.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import PlayerImage from '../components/shared/PlayerImage'
+
+storiesOf('Player Image', module).add('default', () => (
+    <PlayerImage
+        name="Kevin Durant"
+        src="https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201142.png"
+    />
+))

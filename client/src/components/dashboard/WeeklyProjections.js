aab13efdbd75ae240aa9a1165769f0d976ab0d58
Adam Gobran <adamgobes@gmail.com>
2019-09-18 23:09:27 -0400

    finalize WeeklyProjections component

--- client/src/components/dashboard/WeeklyProjections.js
+++ client/src/components/dashboard/WeeklyProjections.js
@@ -1,8 +1,36 @@
-import React from 'react'
-import { Box, Grid } from 'grommet'
+import React, { useState } from 'react'
+import { Box, Select } from 'grommet'
 import styled from 'styled-components'
 
 import basketball from '../../assets/images/basketball.png'
+import assistIcon from '../../assets/images/assist-icon.png'
+import reboundIcon from '../../assets/images/rebound-icon.png'
+import fgIcon from '../../assets/images/effiency.png'
+import spgIcon from '../../assets/images/lock.png'
+import threeIcon from '../../assets/images/three-pointer.png'
+import ftIcon from '../../assets/images/money-bag.png'
+
+const statToIcon = {
+    PPG: basketball,
+    APG: assistIcon,
+    RPG: reboundIcon,
+    SPG: spgIcon,
+    'FG%': fgIcon,
+    'FT%': ftIcon,
+    '3PM': threeIcon,
+}
+
+const ProjectionsContainer = styled(Box)`
+    height: 200px;
+`
+
+const ProjectionsHeader = styled.h1`
+    margin: 30px 20px;
+`
+
+const DropdownContainer = styled.div`
+    width: 140px;
+`
 
 const ProjectionElement = styled(Box)`
     width: 160px;
@@ -15,9 +43,6 @@ const ProjectionElement = styled(Box)`
 const IconWrapper = styled.div`
     width: 40px;
     height: 40px;
-    border: 1px solid;
-    border-radius: 100%;
-    border: 1px solid ${props => props.theme.global.colors.brand};
     overflow: hidden;
 `
 
@@ -26,6 +51,8 @@ const Value = styled.div`
     font-size: 1.2em;
 `
 
+const timeFrames = ['All Time', 'Last 30 Days', 'Last 14 Days', 'Last 7 Days']
+
 function ProjectionsRow({ data }) {
     return (
         <Box direction="row" justify="between">
@@ -33,7 +60,12 @@ function ProjectionsRow({ data }) {
                 <ProjectionElement direction="row" justify="evenly" align="center">
                     <IconWrapper>
                         <Box align="center" justify="center">
-                            <img src={basketball} width="100%" height="100%" alt="PPG" />
+                            <img
+                                src={statToIcon[p.category]}
+                                width="100%"
+                                height="100%"
+                                alt="PPG"
+                            />
                         </Box>
                     </IconWrapper>
                     <Box direction="column">
@@ -47,8 +79,21 @@ function ProjectionsRow({ data }) {
 }
 
 function WeeklyProjections({ showOpponent, myProjections, opponentsProjection }) {
+    const [timeFrame, setTimeFrame] = useState(timeFrames[0])
     return (
-        <Box direction="column" justify="between" align="center" style={{ height: '400px' }}>
+        <ProjectionsContainer direction="column" justify="center">
+            <Box direction="row" align="center">
+                <ProjectionsHeader>Team Averages Per Stat</ProjectionsHeader>
+                <DropdownContainer>
+                    <Select
+                        options={timeFrames}
+                        value={timeFrame}
+                        size="small"
+                        onChange={setTimeFrame}
+                    />
+                </DropdownContainer>
+            </Box>
+
             <Box gridArea="me" direction="row">
                 <ProjectionsRow data={myProjections} />
             </Box>
@@ -62,7 +107,7 @@ function WeeklyProjections({ showOpponent, myProjections, opponentsProjection })
                     </Box>
                 </>
             )}
-        </Box>
+        </ProjectionsContainer>
     )
 }
 

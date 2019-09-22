bc8d40c76ed006486dc69d2e2c6575c5e846bba2
Adam Gobran <adamgobes@gmail.com>
2019-09-22 13:26:34 -0400

    migrate mutation player api to ts

---
+++ server/src/resolvers/Mutation.ts
@@ -0,0 +1,65 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import { ID_Input, User } from '../generated/prisma-client'
+import { APP_SECRET } from '../utils'
+import { getUserId } from '../utils'
+import { GQLAuthPayLoad } from '../generated/gqlTypes'
+
+export async function register(parents, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10)
+
+    const user: User = await context.prisma.createUser({ ...args, password })
+
+    // initialize user's team when they create an account
+    const initialTeam = {
+        name: `${user.name}'s Team`,
+        owner: { connect: { id: user.id } },
+    }
+
+    await context.prisma.createTeam({ ...initialTeam })
+
+    const token: string = jwt.sign({ userId: user.id }, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+export async function login(parent, args, context, info) {
+    const user: User = await context.prisma.user({ email: args.email })
+    if (!user) {
+        throw new Error('No such user found')
+    }
+
+    const valid: boolean = await bcrypt.compare(args.password, user.password)
+    if (!valid) {
+        throw new Error('Invalid password')
+    }
+
+    const token: string = jwt.sign({ userId: user.id }, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+export async function saveTeam(parent, args, context) {
+    const userId: ID_Input = getUserId(context)
+    const teamId: ID_Input = await context.prisma // get user's team based on their id
+        .user({ id: userId })
+        .team()
+        .id()
+
+    /* In DB, Team.players is simply array of sportsFeed IDs
+     * In resolvers/Team.js this array gets mapped to array of actual player objects (see schema.graphql for what that looks like)
+     */
+    return context.prisma.updateTeam({
+        data: {
+            players: { set: args.playerIds },
+        },
+        where: { id: teamId },
+    })
+}
--- server/src/sportsFeed/api.ts
+++ server/src/sportsFeed/api.ts
@@ -1,15 +1,11 @@
-const base64 = require('base-64')
+import base64 from 'base-64'
 
-const { sportsFeedUsername, sportsFeedPassword, sportsFeedUrl } = require('../config')
+import { sportsFeedPassword, sportsFeedUrl, sportsFeedUsername } from '../config'
 
-function sportsFeedRequest(path) {
+export function sportsFeedRequest(path): Promise<any> {
     const pass = base64.encode(`${sportsFeedUsername}:${sportsFeedPassword}`)
     const config = {
         headers: { Authorization: `Basic ${pass}` },
     }
     return fetch(`${sportsFeedUrl}/${path}`, config)
-}
-
-module.exports = {
-    sportsFeedRequest,
 }

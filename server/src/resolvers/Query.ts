c1ffc2108489436247171b428fe4d4974bad69a9
Adam Gobran <adamgobes@gmail.com>
2019-09-22 21:51:14 -0400

    giant refactor of fetchPlayerStats

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,5 +1,5 @@
-import { GQLPlayer, GQLUser, GQLTeam } from '../generated/gqlTypes'
-import { sportsFeedRequest } from '../sportsFeed/api'
+import { GQLPlayer, GQLUser, GQLTeam, GQLLeagueLeader } from '../generated/gqlTypes'
+import { sportsFeedRequest, season, statCategories } from '../sportsFeed/api'
 import { extractBasicInfo } from '../sportsFeed/helpers'
 import { getUserId } from '../utils'
 
@@ -20,17 +20,34 @@ export function me(parent, args, context): GQLUser {
 // resolver to retrieve all players from third-party SportsFeed datasource
 // map through response from API, transform into object that matches schema and finally filter if there was one passed (args.filter)
 export function allPlayers(parent, args): Promise<GQLPlayer[]> {
-    return sportsFeedRequest('players.json')
-        .then(res => res.json())
-        .then(json =>
-            json.players
-                .map(p => extractBasicInfo(p.player))
-                .filter(pObj => (args.filter ? containsFilter(pObj, args.filter) : true))
-        )
+    return sportsFeedRequest('players.json').then(json =>
+        json.players
+            .map(p => extractBasicInfo(p.player))
+            .filter(pObj => (args.filter ? containsFilter(pObj, args.filter) : true))
+    )
 }
 
 export function myTeam(parent, args, context): GQLTeam {
     const id: string = getUserId(context)
 
     return context.prisma.user({ id }).team()
+}
+
+export async function leagueLeaders(parent, args): Promise<any[]> {
+    const allPlayersReq = await sportsFeedRequest('players.json')
+
+    console.log(allPlayersReq)
+
+    return [
+        {
+            stat: {
+                category: 'FGA',
+                value: 100,
+            },
+            leader: {
+                firstName: 'lebron',
+                lastName: 'james',
+            },
+        },
+    ]
 }

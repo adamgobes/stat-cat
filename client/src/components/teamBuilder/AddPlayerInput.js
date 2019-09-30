e779205ba3aaabfa7576a3f3fb50361094bbac11
Adam Gobran <adamgobes@gmail.com>
2019-09-30 16:19:50 -0400

    refactor

--- client/src/components/teamBuilder/AddPlayerInput.js
+++ client/src/components/teamBuilder/AddPlayerInput.js
@@ -8,20 +8,18 @@ const AddPlayer = styled(TextInput)`
     border-color: ${props => props.theme.global.colors.secondary};
 `
 
-const AddPlayerInput = ({ inputValue, onPlayerInputChange }) => {
-    return (
-        <Box direction="row" justify="center">
-            <Box>
-                <AddPlayer
-                    size="medium"
-                    value={inputValue}
-                    onChange={onPlayerInputChange}
-                    placeholder="Search for Players"
-                    focusIndicator={false}
-                />
-            </Box>
+const AddPlayerInput = ({ inputValue, onPlayerInputChange }) => (
+    <Box direction="row" justify="center">
+        <Box>
+            <AddPlayer
+                size="medium"
+                value={inputValue}
+                onChange={onPlayerInputChange}
+                placeholder="Search for Players"
+                focusIndicator={false}
+            />
         </Box>
-    )
-}
+    </Box>
+)
 
 export default AddPlayerInput

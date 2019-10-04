440393f8da126bd2fb5ee4cf4bf345a8c0916ab7
Adam Gobran <adamgobes@gmail.com>
2019-10-03 22:55:44 -0400

    barebones dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,29 +1,8 @@
 import React from 'react'
 import { Box, Grid } from 'grommet'
-import styled from 'styled-components'
-
-import Nav from '../general/Nav'
-
-const WeeklyReportWrapper = styled(Box)`
-    box-shadow: rgba(0, 0, 0, 0.3) 0 1px 6px;
-    background-color: ${props => props.theme.global.colors.brand};
-`
-
-const ProjectionTableWrapper = styled(Box)`
-    box-shadow: rgba(0, 0, 0, 0.3) 0 1px 6px;
-`
-
-const TopPerformersWrapper = styled(Box)`
-    box-shadow: rgba(0, 0, 0, 0.3) 0 1px 6px;
-`
-
-const SomeBox = styled(Box)`
-    height: 200px;
-    width: 200px;
-`
 
 const Dashboard = () => (
-    <Box>
+    <Box style={{ height: '600px' }} align="center" justify="center">
         <h1>Dashboard</h1>
     </Box>
 )

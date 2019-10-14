0104fbe4d8966125d498a2443bb017c3003bf4c8
Adam Gobran <adamgobes@gmail.com>
2019-10-14 11:13:45 -0400

    build initial version of WeeklyOverview

--- client/src/components/dashboard/WeeklyOverview.js
+++ client/src/components/dashboard/WeeklyOverview.js
@@ -1,7 +1,96 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Box } from 'grommet'
+import ReactTooltip from 'react-tooltip'
 
-function WeeklyOverview() {
-    return <h1>weekly overview</h1>
+import PlayerImage from '../shared/PlayerImage'
+
+const WeeklyOverviewWrapper = styled(Box)`
+    width: 800px;
+    height: 400px;
+    background: #eff1f3;
+`
+
+const TableRow = styled(Box)`
+    flex-direction: row;
+    align-items: center;
+    background: white;
+    padding: 10px;
+    margin: 4px 0;
+`
+
+const TableHeader = styled(Box)``
+
+const Table = styled(Box)`
+    width: 96%;
+`
+
+const Entries = styled(Box)``
+
+const Truncated = styled.span`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+function WeeklyOverview({ data }) {
+    return (
+        <WeeklyOverviewWrapper align="center">
+            <ReactTooltip />
+            <Table>
+                <h1>Weekly Overview</h1>
+                <TableRow direction="row">
+                    <Box basis="small">
+                        <TableHeader />
+                    </Box>
+
+                    <Box direction="row" justify="start" basis="small">
+                        <TableHeader>Player Name</TableHeader>
+                    </Box>
+                    <Box
+                        direction="row"
+                        justify="start"
+                        basis="small"
+                        style={{ marginRight: '20px' }}
+                    >
+                        <TableHeader>Injury</TableHeader>
+                    </Box>
+                    <Box direction="row" justify="start" basis="small">
+                        <TableHeader>Playing Probability</TableHeader>
+                    </Box>
+                    <Box direction="row" justify="start" basis="small">
+                        <TableHeader>Games This Week</TableHeader>
+                    </Box>
+                </TableRow>
+                <Entries direction="column">
+                    {data.map(p => (
+                        <TableRow direction="row" align="center">
+                            <Box direction="row" justify="start" basis="small">
+                                <PlayerImage src={p.imageSrc} size="XS" />
+                            </Box>
+                            <Box direction="row" justify="start" basis="small">
+                                {p.fullName}
+                            </Box>
+                            <Box
+                                direction="row"
+                                justify="start"
+                                basis="small"
+                                style={{ marginRight: '20px' }}
+                            >
+                                <Truncated data-tip={p.description}>{p.description}</Truncated>
+                            </Box>
+                            <Box direction="row" justify="start" basis="small">
+                                {p.playingProbability}
+                            </Box>
+                            <Box direction="row" justify="start" basis="small">
+                                2
+                            </Box>
+                        </TableRow>
+                    ))}
+                </Entries>
+            </Table>
+        </WeeklyOverviewWrapper>
+    )
 }
 
 export default WeeklyOverview

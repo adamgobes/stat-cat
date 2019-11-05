765fa4d6233bbcbed1815367f6dfe8af0240cf5c
Adam Gobran <adamgobes@gmail.com>
2019-11-05 15:08:19 -0500

    style SentAndReceived

--- client/src/components/dashboard/SentAndReceived.js
+++ client/src/components/dashboard/SentAndReceived.js
@@ -3,23 +3,40 @@ import styled from 'styled-components'
 import { Box } from 'grommet'
 import PlayerImage from '../shared/PlayerImage'
 
+const ComponentWrapper = styled(Box)`
+    min-height: 300px;
+    background: white;
+    margin: 10px 0;
+    border-radius: 10px;
+`
+
 const PlayerWrapper = styled(Box)`
     background: white;
-    margin: 10px 10px;
+    margin: 2px 10px;
     padding: 10px;
     border-radius: 10px;
 `
 
+const Truncated = styled.h3`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-left: 6px;
+    width: 60%;
+`
+
 export default function SentAndReceived({ title, players }) {
     return (
-        <Box direction="column">
-            <h2>{title}</h2>
+        <ComponentWrapper direction="column">
+            <h2 style={{ margin: '12px' }}>{title}</h2>
             {players.map(player => (
                 <PlayerWrapper direction="row" align="center" basis="1/2">
                     <PlayerImage src={player.imageSrc} size="XS" />
-                    <h3>{`${player.firstName.substring(0, 1)}. ${player.lastName}`}</h3>
+                    <Truncated>
+                        {`${player.firstName.substring(0, 1)}. ${player.lastName}`}
+                    </Truncated>
                 </PlayerWrapper>
             ))}
-        </Box>
+        </ComponentWrapper>
     )
 }

4cf1e69eb24fb903f67bbccdd5d8951b28e70422
Adam Gobran <adamgobes@gmail.com>
2019-11-29 18:36:35 -0500

    fallback message uses text component

--- client/src/components/general/FallbackMessage.js
+++ client/src/components/general/FallbackMessage.js
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { Box, Button } from 'grommet'
 
+import { Title } from './TextComponents'
 import StatCatLogo from './Logo'
 
-const ErrorMessage = styled.h1`
+const ErrorMessage = styled(Title)`
+    color: black;
     text-align: center;
     line-height: 1.2;
     width: 70%;

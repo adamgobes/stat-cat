8ea30667a2a203b0454f8a1e66b8b11105071f42
Adam Gobran <adamgobes@gmail.com>
2019-12-06 13:57:32 -0500

    pushing to master: change env vars

--- client/src/components/general/Contact.js
+++ client/src/components/general/Contact.js
@@ -43,10 +43,10 @@ export default function Contact() {
             email,
         }
         send(
-            process.env.EMAILJS_SERVICE_ID,
-            process.env.EMAILJS_TEMPLATE_ID,
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
             templateParams,
-            process.env.EMAILJS_USER_ID
+            process.env.REACT_APP_EMAILJS_USER_ID
         )
             .then(() => {
                 setResult('Thanks! We got your message and will be in touch soon!')

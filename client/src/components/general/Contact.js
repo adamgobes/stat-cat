1822c518cfef011d54729afa206ee1c5980c2132
Adam Gobran <adamgobes@gmail.com>
2019-12-06 13:09:16 -0500

    integrate emailjs

--- client/src/components/general/Contact.js
+++ client/src/components/general/Contact.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Box, TextInput, TextArea } from 'grommet'
-import { Title, Text } from './TextComponents'
+import { send } from 'emailjs-com'
+
+import { Title, Text, Subheader } from './TextComponents'
 import HomeNav from '../home/HomeNav'
 import { RoundedButton } from './Buttons'
+import Loader from '../shared/Loader'
 
 const ContactForm = styled(Box)``
 
@@ -22,6 +25,8 @@ export default function Contact() {
     const [email, setEmail] = useState('')
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
+    const [loading, setLoading] = useState('')
+    const [result, setResult] = useState('Thanks! We got your message and will be in touch soon!')
 
     function formValid() {
         return (
@@ -29,44 +34,80 @@ export default function Contact() {
         )
     }
 
+    function handleFormSubmit() {
+        setLoading(true)
+        const templateParams = {
+            subject,
+            name,
+            message,
+            email,
+        }
+        send('adam_gobran', 'template_5IRWoQks', templateParams, 'user_ndpNcHAqxdL0sF1D25sVn')
+            .then(() => {
+                setResult('Thanks! We got your message and will be in touch soon!')
+                setLoading(false)
+            })
+            .catch(() => {
+                setResult(
+                    'Oops! Looks like something went wrong sending the form! Try again in a few minutes'
+                )
+            })
+    }
+
     return (
         <Box pad="large" justify="center" align="center">
             <HomeNav showButtons={false} />
-            <Title style={{ marginTop: '40px' }}>Love statcat? Hate it? Want a new feature?</Title>
-            <Text style={{ margin: '-10px 0 10px 0', fontSize: '1.3em' }}>Drop us a line</Text>
-            <ContactForm>
-                <FormInput
-                    size="medium"
-                    name="name"
-                    placeholder="Name"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-                <FormInput
-                    size="medium"
-                    name="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <FormInput
-                    size="medium"
-                    name="subject"
-                    placeholder="Subject"
-                    value={subject}
-                    onChange={e => setSubject(e.target.value)}
-                />
-                <MessageWrapper>
-                    <TextArea
-                        fill
-                        name="message"
-                        placeholder="Leave us your message here"
-                        value={message}
-                        onChange={e => setMessage(e.target.value)}
-                    />
-                </MessageWrapper>
-                <RoundedButton inverted label="Submit" disabled={!formValid()} />
-            </ContactForm>
+            {result.length === 0 && (
+                <>
+                    <Title style={{ marginTop: '40px' }}>
+                        Love statcat? Hate it? Want a new feature?
+                    </Title>
+                    <Text style={{ margin: '-10px 0 10px 0', fontSize: '1.3em' }}>
+                        Drop us a line and get in touch
+                    </Text>
+                    <ContactForm>
+                        <FormInput
+                            size="medium"
+                            name="name"
+                            placeholder="Name"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                        <FormInput
+                            size="medium"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <FormInput
+                            size="medium"
+                            name="subject"
+                            placeholder="Subject"
+                            value={subject}
+                            onChange={e => setSubject(e.target.value)}
+                        />
+                        <MessageWrapper>
+                            <TextArea
+                                fill
+                                name="message"
+                                placeholder="Leave us your message here"
+                                value={message}
+                                onChange={e => setMessage(e.target.value)}
+                            />
+                        </MessageWrapper>
+                        <RoundedButton
+                            inverted
+                            label={loading ? <Loader size={20} /> : 'Submit'}
+                            disabled={!formValid()}
+                            onClick={() => handleFormSubmit()}
+                        />
+                    </ContactForm>
+                </>
+            )}
+            {result.length !== 0 && (
+                <Title style={{ marginTop: '90px', fontSize: '1.4em' }}>{result}</Title>
+            )}
         </Box>
     )
 }

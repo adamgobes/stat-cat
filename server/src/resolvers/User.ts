5c14749a39046cdec0d6c500e39796798c5859aa
Adam Gobran <adamgobes@gmail.com>
2019-12-21 13:42:51 -0500

    Update queries multiple teams (#137)
    * refactor myTeam to support multiple teams
    
    * update saveTeam mutation

--- server/src/resolvers/User.ts
+++ server/src/resolvers/User.ts
@@ -4,5 +4,5 @@ import { GQLTeam } from '../generated/gqlTypes'
 export function teams(parent, args, context): GQLTeam {
     const id: string = getUserId(context)
 
-    return context.prisma.user({ id }).teams()[0]
+    return context.prisma.user({ id }).teams()
 }

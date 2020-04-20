5cb025a89aa425f993548b35ce24f3b99c0cbc12
Adam Gobran <adamgobes@gmail.com>
2020-04-19 21:03:21 -0400

    write add players test

--- server/src/resolvers/resolvers.test.ts
+++ server/src/resolvers/resolvers.test.ts
@@ -1,11 +1,13 @@
 import { Prisma, models } from '../generated/prisma-client/index'
 import { typeDefs } from '../generated/prisma-client/prisma-schema'
-import { register, login } from './Mutation'
+import { register, login, saveTeam } from './Mutation'
+import { players } from './Team'
 
 const prismaInstance: Prisma = new Prisma()
 
 const TEST_EMAIL: string = 'test@gmail.com'
 let testTeamId: string
+let authToken: string
 
 afterAll(async () => {
     await prismaInstance.deleteTeam({ id: testTeamId })
@@ -37,7 +39,30 @@ describe('resolvers', () => {
         }
 
         const { token: loginToken } = await login(parent, loginArgs, ctx, info)
+        authToken = loginToken
 
         expect(loginToken).not.toBeNull()
+    })
+
+    it('adds players to the users team', async () => {
+        const parent = {}
+        const ctx = {
+            prisma: prismaInstance,
+            request: {
+                get: () => `Bearer ${authToken}`,
+            },
+        }
+        const info = {}
+
+        const sampleIds: string[] = ['9158', '9369', '9232', '9387']
+
+        const addPlayerArgs = {
+            teamId: testTeamId,
+            playerIds: sampleIds,
+        }
+
+        const { players: playerIds } = await saveTeam(parent, addPlayerArgs, ctx)
+
+        expect(playerIds).toEqual(sampleIds)
     })
 })
